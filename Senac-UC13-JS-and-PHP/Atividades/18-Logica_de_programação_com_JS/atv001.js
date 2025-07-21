7ddf8d05be22ffc708509b004238fadf6e3f035d
Yuri-Miranda-Nunes <yurimirandanunes1@gmail.com>
2025-07-21 11:16:05 -0300

    Add atv001.js with Pessoa and Carro objects for demonstration

---
+++ Senac-UC13-JS-and-PHP/Atividades/18-Logica_de_programação_com_JS/atv001.js
@@ -0,0 +1,25 @@
+let Pessoa = {
+    nome: "Yuri",
+    sexo: "Masculino",
+    idade: 20,
+    salario: 1000
+};
+
+console.log("Nome: " + Pessoa.nome);
+console.log("Sexo: " + Pessoa.sexo);
+console.log("Idade: " + Pessoa.idade);
+console.log("Salário: " + Pessoa.salario);
+
+let Carro = {
+    marca: "Fiat",
+    modelo: "Uno",
+    anoFabricação: 2020,
+    anoModelo: 2021,
+    valor: 30000
+};
+
+console.log("Marca: " + Carro.marca);
+console.log("Modelo: " + Carro.modelo);
+console.log("Ano de Fabricação: " + Carro.anoFabricação);
+console.log("Ano do Modelo: " + Carro.anoModelo);
+console.log("Valor: " + Carro.valor);
